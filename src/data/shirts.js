4ce78d336d523e772d70e9d56694f8c3cd009455
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-11 21:47:26 +0100

    Implement static json

---
+++ src/data/shirts.js
@@ -0,0 +1,77 @@
+import shirt1 from "../assets/images/polo-manga-larga-vestir-nino_id_11-04161-061-M-4.jpg";
+import shirt2 from "../assets/images/polo-manga-larga-print-nino_id_11-04159-031-M-4.jpg";
+import shirt3 from "../assets/images/polo-manga-larga-combinado-nino_id_11-04154-066-M-4.jpg";
+import shirt4 from "../assets/images/polo-ecofriends-manga-larga-skate-nino_id_11-04155-071-M-4.jpg";
+import shirt5 from "../assets/images/polo-manga-larga-print-nino_id_11-04159-030-M-4.jpg";
+import shirt6 from "../assets/images/polo--ecofriends-manga-larga-estampado-nino_id_11-04158-097-M-4.jpg";
+import shirt7 from "../assets/images/polo-ecofriends-manga-larga-skate-nino_id_11-04155-072-M-4.jpg";
+import shirt8 from "../assets/images/polo-rugby-banda-diagonal--nino_id_11-55121-701-M-4.jpg";
+import shirt9 from "../assets/images/polo-ecofriends--manga-larga-nino_id_11-04160-054-M-4.jpg";
+import shirt10 from "../assets/images/polo-manga-larga-desagujado-blocking-nino_id_11-55125-701-M-4.jpg";
+
+const shirts = [
+  {
+    image: shirt1,
+    title: "Polo manga larga vestir niño",
+    price: 24.99,
+    discount: 0,
+  },
+  {
+    image: shirt2,
+    title: "Polo manga larga print niño",
+    price: 22.99,
+    discount: 0,
+  },
+  {
+    image: shirt3,
+    title: "Polo manga larga combinado niño",
+    price: 22.99,
+    discount: 0,
+  },
+  {
+    image: shirt4,
+    title: "Polo ECOFRIENDS manga larga skate niño",
+    price: 24.99,
+    discount: 0,
+  },
+  {
+    image: shirt5,
+    title: "Polo manga larga print niño",
+    price: 22.99,
+    discount: 0,
+  },
+  {
+    image: shirt6,
+    title: "Polo ECOFRIENDS manga larga estampado niño",
+    price: 22.99,
+    discount: 0,
+  },
+  {
+    image: shirt7,
+    title: "Polo ECOFRIENDS manga larga skate niño",
+    price: 22.99,
+    discount: 0,
+  },
+  {
+    image: shirt8,
+    title: "Polo rugby banda diagonal niño",
+    price: 22.99,
+    discount: 0,
+  },
+  {
+    image: shirt9,
+    title: "Polo ECOFRIENDS manga larga niño",
+    price: 22.99,
+    discount: 0,
+  },
+  {
+    image: shirt10,
+    title: "Polo manga larga desagujado blocking niño",
+    price: 24.99,
+    discount: 0,
+  },
+];
+
+const json = JSON.stringify(shirts);
+
+export default json;
