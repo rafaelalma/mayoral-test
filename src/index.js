afa05384731f40ae1146c2226ec8b9324d1360df
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-12 00:42:22 +0100

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+import Search from "./views/Search";
+import Detail from "./views/Detail";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="search" element={<Search />} />
+          <Route path="detail/:id" element={<Detail />} />
+          <Route path="*" element={<h2>404</h2>} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
