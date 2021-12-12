5024f3639084493b243894b4949e56c90e81e7ed
Rafael Álvarez <rafaelalma92@gmail.com>
2021-12-12 14:32:27 +0100

    Move images to public folder and update json

--- src/data/shirts.js
+++ src/data/shirts.js
@@ -1,18 +1,9 @@
-import shirt1 from "../assets/images/polo-manga-larga-vestir-nino_id_11-04161-061-M-4.jpg";
-import shirt2 from "../assets/images/polo-manga-larga-print-nino_id_11-04159-031-M-4.jpg";
-import shirt3 from "../assets/images/polo-manga-larga-combinado-nino_id_11-04154-066-M-4.jpg";
-import shirt4 from "../assets/images/polo-ecofriends-manga-larga-skate-nino_id_11-04155-071-M-4.jpg";
-import shirt5 from "../assets/images/polo-manga-larga-print-nino_id_11-04159-030-M-4.jpg";
-import shirt6 from "../assets/images/polo--ecofriends-manga-larga-estampado-nino_id_11-04158-097-M-4.jpg";
-import shirt7 from "../assets/images/polo-ecofriends-manga-larga-skate-nino_id_11-04155-072-M-4.jpg";
-import shirt8 from "../assets/images/polo-rugby-banda-diagonal--nino_id_11-55121-701-M-4.jpg";
-import shirt9 from "../assets/images/polo-ecofriends--manga-larga-nino_id_11-04160-054-M-4.jpg";
-import shirt10 from "../assets/images/polo-manga-larga-desagujado-blocking-nino_id_11-55125-701-M-4.jpg";
-
 const shirts = [
   {
     id: 1,
-    image: shirt1,
+    image:
+      process.env.PUBLIC_URL +
+      "/assets/images/polo-manga-larga-vestir-nino_id_11-04161-061-M-4.jpg",
     title: "Polo manga larga vestir niño",
     price: 24.99,
     discount: 0.2,
@@ -20,7 +11,9 @@ const shirts = [
   },
   {
     id: 2,
-    image: shirt2,
+    image:
+      process.env.PUBLIC_URL +
+      "/assets/images/polo-manga-larga-print-nino_id_11-04159-031-M-4.jpg",
     title: "Polo manga larga print niño",
     price: 22.99,
     discount: 0,
@@ -28,7 +21,9 @@ const shirts = [
   },
   {
     id: 3,
-    image: shirt3,
+    image:
+      process.env.PUBLIC_URL +
+      "/assets/images/polo-manga-larga-combinado-nino_id_11-04154-066-M-4.jpg",
     title: "Polo manga larga combinado niño",
     price: 22.99,
     discount: 0.1,
@@ -36,7 +31,9 @@ const shirts = [
   },
   {
     id: 4,
-    image: shirt4,
+    image:
+      process.env.PUBLIC_URL +
+      "/assets/images/polo-ecofriends-manga-larga-skate-nino_id_11-04155-071-M-4.jpg",
     title: "Polo ECOFRIENDS manga larga skate niño",
     price: 24.99,
     discount: 0,
@@ -44,7 +41,9 @@ const shirts = [
   },
   {
     id: 5,
-    image: shirt5,
+    image:
+      process.env.PUBLIC_URL +
+      "/assets/images/polo-manga-larga-print-nino_id_11-04159-030-M-4.jpg",
     title: "Polo manga larga print niño",
     price: 22.99,
     discount: 0.3,
@@ -52,7 +51,9 @@ const shirts = [
   },
   {
     id: 6,
-    image: shirt6,
+    image:
+      process.env.PUBLIC_URL +
+      "/assets/images/polo--ecofriends-manga-larga-estampado-nino_id_11-04158-097-M-4.jpg",
     title: "Polo ECOFRIENDS manga larga estampado niño",
     price: 22.99,
     discount: 0,
@@ -60,7 +61,9 @@ const shirts = [
   },
   {
     id: 7,
-    image: shirt7,
+    image:
+      process.env.PUBLIC_URL +
+      "/assets/images/polo-ecofriends-manga-larga-skate-nino_id_11-04155-072-M-4.jpg",
     title: "Polo ECOFRIENDS manga larga skate niño",
     price: 22.99,
     discount: 0,
@@ -68,7 +71,9 @@ const shirts = [
   },
   {
     id: 8,
-    image: shirt8,
+    image:
+      process.env.PUBLIC_URL +
+      "/assets/images/polo-rugby-banda-diagonal--nino_id_11-55121-701-M-4.jpg",
     title: "Polo rugby banda diagonal niño",
     price: 22.99,
     discount: 0.2,
@@ -76,7 +81,9 @@ const shirts = [
   },
   {
     id: 9,
-    image: shirt9,
+    image:
+      process.env.PUBLIC_URL +
+      "/assets/images/polo-ecofriends--manga-larga-nino_id_11-04160-054-M-4.jpg",
     title: "Polo ECOFRIENDS manga larga niño",
     price: 22.99,
     discount: 0,
@@ -84,7 +91,9 @@ const shirts = [
   },
   {
     id: 10,
-    image: shirt10,
+    image:
+      process.env.PUBLIC_URL +
+      "/assets/images/polo-manga-larga-desagujado-blocking-nino_id_11-55125-701-M-4.jpg",
     title: "Polo manga larga desagujado blocking niño",
     price: 24.99,
     discount: 0,
